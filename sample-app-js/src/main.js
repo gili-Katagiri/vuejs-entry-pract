98113554d584f33de181489ce2fd0f39592e9e6f
gili-Katagiri <kt.giri1425@gmail.com>
2021-02-06 10:37:34 +0900

    ready to use vuetify and mockdata.

--- sample-app-js/src/main.js
+++ sample-app-js/src/main.js
@@ -6,11 +6,15 @@ Vue.use(VueCompositionAPI);
 import App from '@/App.vue';
 import router from '@/router';
 import store from '@/store';
+import vuetify from '@/plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App),
 }).$mount('#app');
--- sample-app-js/vue.config.js
+++ sample-app-js/vue.config.js
@@ -8,4 +8,6 @@ module.exports = {
       }),
     ],
   },
+
+  transpileDependencies: ['vuetify'],
 };
